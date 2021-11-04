5aa760e57aa95415a526f4f0d316296d7400945e
Christoph Reiter <reiter.christoph@gmail.com>
2021-11-04 10:59:59 +0100

    CI: write out the AT rules metadata as a table
    This should make it easier to track things

---
+++ rules.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import json
+import sys
+from tabulate import tabulate
+
+def main(argv):
+    with open(argv[1], "rb") as h:
+        rules = json.loads(h.read())
+
+    all_ = []
+    keys = []
+
+    for r in rules["r"]:
+        rule = json.loads(r["r"])
+        if rule["Country"] == "AT":
+
+            rule.pop("Logic", None)
+            rule.pop("AffectedFields", None)
+            
+            for desc in list(rule["Description"]):
+                if desc["lang"] == "en":
+                    rule["Description"] = desc["desc"]
+
+            all_.append(list(rule.values()))
+            keys = list(rule.keys())
+    all_.sort(key=lambda e: (e[-1], e[-3], e[0]), reverse=True)
+    print(tabulate(all_, keys, tablefmt="github"))
+
+if __name__ == "__main__":
+    main(sys.argv)
